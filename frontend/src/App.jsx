8a8d6df011dd92912a56989396cd3c52a392037f
danye <ye.da@northeastern.edu>
2023-12-12 00:33:45 -0800

    json2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // App.jsx
 
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
